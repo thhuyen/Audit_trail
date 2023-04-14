d474a85f48bc9edf5528985986182e783d19842e
Thanh Huyền Nguyễn <20520207@gm.uit.edu.vn>
2023-04-14 12:41:41 +0700

    update log

--- del_log.php
+++ del_log.php
@@ -4,6 +4,8 @@
     if (isset($_POST['btn-del'])) {
         if (isset($_COOKIE['logID'])) {
             mysqli_query($conn, "DELETE FROM `logs` WHERE id = '".$_COOKIE['logID']."'");
+            mysqli_query($conn, "INSERT INTO `logs`(`id`, `action`, `date`, `username`) 
+                                VALUES ('0','delete log having id - {$_COOKIE['logID']}','$date','huyen')");
         }
     }
     Header("Location: ./logs.php");
--- update_log.php
+++ update_log.php
@@ -8,6 +8,8 @@
     if (isset($_POST['btn-save-update'])) {
         echo 1;
         mysqli_query($conn,"UPDATE `logs` SET `action`='$action',`username`='$username' WHERE id = '$id'");
+        mysqli_query($conn, "INSERT INTO `logs`(`id`, `action`, `date`, `username`) 
+                                VALUES ('0','update log having id - {$id}','$date','huyen')");
     }
     Header("Location: ./logs.php");
     
