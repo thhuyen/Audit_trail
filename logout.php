88f0b65e843e2e74db7393c53b1520755437a540
Thanh Huyền Nguyễn <20520207@gm.uit.edu.vn>
2023-04-15 17:25:48 +0700

    final

--- logout.php
+++ logout.php
@@ -1,3 +1,19 @@
-<button onclick="myFunction()" >
-    <i class="dropbtn fa-solid fa-arrow-right-from-bracket"></i>
-</button>
+<?php
+    include './connect.php';
+    if (isset($_POST['btn-logout'])) {
+        date_default_timezone_set('Asia/Ho_Chi_Minh');
+        $date = date('y-m-d h:i:s');
+        $username = $_GET['username'];
+        mysqli_query($conn, "INSERT INTO `logs`(`id`, `action`, `date`, `username`) 
+                                VALUES ('0','logout system','$date','{$username}')");
+        Header("Location: ./login.php");
+    }
+
+?>
+
+<form action="#" method="post">
+    <button type="submit" name="btn-logout">
+        <i class="dropbtn fa-solid fa-arrow-right-from-bracket"></i>
+    </button>
+</form>
+
